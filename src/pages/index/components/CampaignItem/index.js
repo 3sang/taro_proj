59abe51595bc36f3e94c6c99ac3d8f33957f03e2
saraku <1285480439@qq.com>
2021-10-18 17:44:01 +0800

    详情

--- src/pages/index/components/CampaignItem/index.js
+++ src/pages/index/components/CampaignItem/index.js
@@ -9,16 +9,22 @@ import styles from './index.module.less'
 @observer
 export default class Index extends Component {
 
+  /** 查看活动详情 */
+  onClick = () =>{
+    const {data} = this.props;
+    Taro.navigateTo({url:`/pages/campaign/index?campaignId=${data.id}`})
+  }
+
   render () {
     const {data} = this.props;
     return (
       <View className={styles.container}>
         <View className={styles.header}>
-          <View>{data.name}</View>
+          <View onClick={this.onClick}>{data.name}</View>
           <View>参与人数：{data.enterNumber}</View>
           <View>活动时间：{getWeekTime(data.beginTime)} 至 {getWeekTime(data.endTime)}</View>
         </View>
-        <Image src={`http://localhost:8081/images/data/${data.cover}`} />
+        <Image onClick={this.onClick} src={`http://localhost:8081/images/data/${data.cover}`} />
         <Text className={styles.desc}>{data.description}</Text>
       </View>
     )
